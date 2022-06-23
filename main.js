a46c1a88b543ec507d3f0c6a13e835f06c27d807
minnuss <ponjevic.goran@hotmail.com>
2022-06-23 21:01:10 +0200

    cors update

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 let img = document.querySelector('.image')
 let pictureBox = document.querySelector('.pic-box')
 
-// create async await fetch pictures function
 async function getPictures(i) {
     let URL = `https://img.elmaz.com/uploads/img/00/05/48/47/67/5484767/5484767-${i}-rr.jpg?si=8147857`
-    let response = await fetch(URL)
-    let data = await response.json()
-    console.log('data', data)
-    let img = document.createElement('img')
-    img.src = data.url
-    pictureBox.appendChild(img)
+    try {
+        const response = await fetch(URL, {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+            },
+        });
 
-    createPictureBox(data.url)
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log('data', data)
+        let img = document.createElement('img')
+        img.src = data.url
+        pictureBox.appendChild(img)
+
+        createPictureBox(data.url)
+    } catch (err) {
+        console.log(err);
+    }
 }
+
+
+// create async await fetch pictures function
+// async function getPictures(i) {
+//     let URL = `https://img.elmaz.com/uploads/img/00/05/48/47/67/5484767/5484767-${i}-rr.jpg?si=8147857`
+//     let response = await fetch(URL)
+//     let data = await response.json()
+//     console.log('data', data)
+//     let img = document.createElement('img')
+//     img.src = data.url
+//     pictureBox.appendChild(img)
+
+//     createPictureBox(data.url)
+// }
 
 for (let i = 1; i <= 20; i++) {  // loop through 20 pictures
     getPictures(i)
